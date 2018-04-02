bfd0b96def8ebac1a7a7f9ba901ac4125326d66e
tomasroaldsnes <tomas.roaldsnes@gmail.com>
2018-04-02 11:30:14 +1000

    Learned state

--- src/index.js
+++ src/index.js
@@ -25,22 +25,37 @@ class Counter extends React.Component {
     this.handleAdd = this.handleAdd.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count : 0
+    };
   }
   handleAdd(){
-    alert('+1');
+    this.setState((prevState) =>{
+      return {
+        count: prevState.count + 1
+      };
+    });
   }
 
   handleRemove(){
-    alert('-1');
+    this.setState((prevState) =>{
+      return {
+        count: prevState.count - 1
+      };
+    });
   }
 
   handleReset(){
-    alert('reset');
+    this.setState((prevState) =>{
+      return {
+        count: 0
+      };
+    });
   }
   render() {
 
     return <div>
-    <h1>Count: </h1>
+    <h1>Count: {this.state.count} </h1>
     <button onClick={this.handleAdd}>+1</button>
     <button onClick={this.handleRemove}>-1</button>
     <button onClick={this.handleReset}>Reset</button>
