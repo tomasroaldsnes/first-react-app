76c75d61af00788b2ae9c25ca69d0e65c10199b6
tomasroaldsnes <tomas.roaldsnes@gmail.com>
2018-04-10 16:22:49 +1000

    added scss functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './App.js';
 import registerServiceWorker from './registerServiceWorker';
+import './index.scss';
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('root'));
 
